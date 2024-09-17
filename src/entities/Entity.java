a067f42a0bd1895e1fdcc8562189e8c9840bc9fa
Anthony <anthony.pradier04@gmail.com>
2024-09-17 17:55:47 +0200

    Update of the hitbox drawing, added a spritebox and moved the DrawOffset coordinate to Entity class, which could be in the constructor

--- src/entities/Entity.java
+++ src/entities/Entity.java
@@ -1,11 +1,15 @@
 package entities;
 
+import main.Game;
+
 import java.awt.*;
+import java.awt.geom.Rectangle2D;
 
 public abstract class Entity {
     protected float aX, aY;
     protected int aWidth, aHeight;
-    protected Rectangle aHitbox;
+    protected Rectangle2D.Float aSpriteBox;
+    protected Rectangle2D.Float aHitbox;
 
     public Entity(final float pX, final float pY, final int pWidth, final int pHeight) {
         this.aX = pX;
@@ -13,27 +17,40 @@ public abstract class Entity {
         this.aWidth = pWidth;
         this.aHeight = pHeight;
 
-        this.initHitbox();
+        this.initSpriteBox();
     }
 
     protected void drawHitbox(final Graphics pG) {
         // For debugging hitbox
         pG.setColor(Color.pink);
-        pG.drawRect(this.aHitbox.x, this.aHitbox.y, this.aHitbox.width, this.aHitbox.height);
+        pG.drawRect((int)this.aHitbox.x, (int)this.aHitbox.y, (int)this.aHitbox.width, (int)this.aHitbox.height);
     }
 
-    private void initHitbox() {
-        this.aHitbox = new Rectangle((int)this.aX, (int)this.aY, this.aWidth, this.aHeight);
+    protected void drawSpriteBox(final Graphics pG) {
+        pG.setColor(Color.RED);
+        pG.drawRect((int)this.aX, (int)this.aY, (int)(Game.PLAYER_SPRITE_SIZE * Game.SCALE), (int)(Game.PLAYER_SPRITE_SIZE * Game.SCALE));
     }
 
-    protected void updateHitbox() {
+    protected void initHitbox(final float pX, final float pY, final float pWidth, final float pHeight) {
+        this.aHitbox = new Rectangle2D.Float(pX, pY, pWidth, pHeight);
+    }
+
+    protected void initSpriteBox() {
+        this.aSpriteBox = new Rectangle2D.Float(this.aX, this.aY, Game.PLAYER_SPRITE_SIZE * Game.SCALE, Game.PLAYER_SPRITE_SIZE * Game.SCALE);
+    }
+
+    /*protected void updateHitbox() {
         this.aHitbox.x = (int)this.aX;
         this.aHitbox.y = (int)this.aY;
 
+    }*/
+
+    public Rectangle2D.Float getHitbox() {
+        return this.aHitbox;
     }
 
-    public Rectangle getHitbox() {
-        return this.aHitbox;
+    public Rectangle2D.Float getSpriteBox() {
+        return this.aSpriteBox;
     }
 
     public void update() {}
